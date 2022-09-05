13072d56324cfbae5ac9d0ad0f9c7cb6f30d94da
margosq <slava1kvartovkin@gmail.com>
2022-09-05 13:52:49 +0300

    feat: lexer probably works now

--- srcs/parse/lexer.c
+++ srcs/parse/lexer.c
@@ -1,46 +1,27 @@
 #include "minishell.h"
 
-int	get_code(char sy, int len)
+int	get_code(int sy, int len)
 {
-	switch ( (int)sy )
-	{
-		case s_quote:
-			return (s_quote_str);
-		case d_quote_str:
-			return (s_quote_str);
-		case (l_corner):
-			if (len == 1)
-				return (r_in);
-			else if (len == 2)
-				return (heredoc);
-		case (r_corner):
-			if (len == 1)
-				return (r_out);
-			else if (len == 2)
-				return (r_append);
-	}
+	if (sy == s_quote)
+		return (s_quote_str);
+	else if (sy == d_quote)
+		return (d_quote_str);
+	else if (sy == l_corner && len == 1)
+		return (r_in);
+	else if (sy == l_corner && len == 2)
+		return (heredoc);
+	else if (sy == r_corner && len == 1)
+		return (r_out);
+	else if (sy == r_corner && len == 2)
+		return (r_append);
 	return (word);
-	// if ((int)sy == s_quote)
-	// else if ((int)sy == d_quote)
-	// 	return (d_quote_str);
-	// else if ((int)sy == l_corner && len == 1)
-	// else if ((int)sy == l_corner && len == 2)
-	// 	return (heredoc);
-	// else if ((int)sy == r_corner && len == 1)
-	// 	return (r_out);
-	// else if ((int)sy == r_corner && len == 2)
-	// 	return (r_append);
 }
 
-static inline int	move(char sy, int len)
+static inline int	get_skip_distance(int sy, int len)
 {
-	switch ((int)sy)
-	{
-	case s_quote: case d_quote:
-		return (len + 1);
-	default:
-		return (len);
-	}
+	if (sy == s_quote || sy == d_quote)
+		return (len + 2);
+	return (len);
 }
 
 t_token	*lex_line(char *line)
@@ -50,7 +31,6 @@ t_token	*lex_line(char *line)
 	int		code;
 	int		until;
 	t_token	*tokens;
-	// t_token	*tmp;
 
 	i = 0;
 	tokens = NULL;
@@ -63,9 +43,9 @@ t_token	*lex_line(char *line)
 		else
 		{
 			len = find_end(&line[i]);
-			code = get_code(line[i], len);
+			code = get_code((int)line[i], len);
 			tokenlst_add_back(&tokens, tokenlst_new(&line[i], len, code));
-			i+=move(code, len);
+			i += get_skip_distance(line[i], len);
 		}
 	}
 	tokenlst_print(tokens);
