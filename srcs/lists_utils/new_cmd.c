ed54db6e77b5e9e5b3f1dd00e0defb8508d42424
margosq <slava1kvartovkin@gmail.com>
2022-10-12 16:32:20 +0300

    fix: last file parser

--- srcs/lists_utils/new_cmd.c
+++ srcs/lists_utils/new_cmd.c
@@ -3,32 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   new_cmd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptoshiko <ptoshiko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svyatoslav <svyatoslav@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:49:02 by ptoshiko          #+#    #+#             */
-/*   Updated: 2022/10/10 18:18:43 by ptoshiko         ###   ########.fr       */
+/*   Updated: 2022/10/12 16:26:26 by svyatoslav       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	else_fetch_last_file(t_list	*tokenlst)
-{
-	t_token			*last;
-	t_token_type	code;
-
-	while (tokenlst)
-	{
-		if (((t_token *)(tokenlst->content))->code == r_out)
-		{
-			last = (t_token *)(tokenlst->next->content);
-			code = ((t_token *)(tokenlst->content))->code;
-		}
-		tokenlst = tokenlst->next;
-	}
-}
-
-t_file	*if_last_fetch_last_file(t_token_type code, t_token *last)
+static t_file	*create_file(t_token_type code, t_token *last)
 {
 	t_file			*file;
 
@@ -41,6 +25,20 @@ t_file	*if_last_fetch_last_file(t_token_type code, t_token *last)
 	return (file);
 }
 
+static void	find_file(short type, t_list *tokenlst,
+	t_token_type *code, t_token **last)
+{
+	t_token	*token;
+
+	token = (tokenlst->content);
+	if ((token->code == r_in) && type == 1
+		|| (token->code == r_out || token->code == r_append) && type == 0)
+	{
+		*last = (t_token *)(tokenlst->next->content);
+		*code = ((t_token *)(tokenlst->content))->code;
+	}
+}
+
 static t_file	*fetch_last_file(t_list	*tokenlst, short type)
 {
 	t_token			*last;
@@ -49,23 +47,13 @@ static t_file	*fetch_last_file(t_list	*tokenlst, short type)
 
 	last = NULL;
 	file = NULL;
-	if (type == 1)
+	while (tokenlst)
 	{
-		while (tokenlst)
-		{
-			if (((t_token *)(tokenlst->content))->code == r_in
-				|| ((t_token *)(tokenlst->content))->code == r_append)
-			{
-				last = (t_token *)(tokenlst->next->content);
-				code = ((t_token *)(tokenlst->content))->code;
-			}
-			tokenlst = tokenlst->next;
-		}
+		find_file(type, tokenlst, &code, &last);
+		tokenlst = tokenlst->next;
 	}
-	else
-		else_fetch_last_file(tokenlst);
 	if (last)
-		file = if_last_fetch_last_file(code, last);
+		file = create_file(code, last);
 	return (file);
 }
 
