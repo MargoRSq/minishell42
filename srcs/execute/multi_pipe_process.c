839f3921f54e33b99bfacdd1f1b14e169874d862
margosq <slava1kvartovkin@gmail.com>
2022-10-12 15:49:59 +0300

    Merge branch 'fix/multi-pipes' into develop

--- srcs/execute/multi_pipe_process.c
+++ srcs/execute/multi_pipe_process.c
@@ -1,83 +1,26 @@
 #include "minishell.h"
 
-// int	cmd_len(t_cmd *cmd)
-// {
-// 	t_cmd	*tmp;
-// 	int		len;
-
-// 	len = 0;
-// 	tmp = cmd;
-// 	while (tmp)
-// 	{
-// 		len++;
-// 		tmp = tmp->next;
-// 	}
-// 	return len;
-// }
-
-//int	**make_pipe_space(int **pipes, int len)
-//{
-//	int i;
-//
-//	pipes = malloc(sizeof(int *) * (len - 1));
-//	if (!pipes)
-//		return (error_msg_return_int(MSG_ERR_MEM, NULL, malloc_error,
-//											1));
-////		trigger_malloc_error();//need to clarify!
-//	i = 0;
-//	while (i < (len - 1))
-//	{
-//		pipes[i] = malloc(sizeof(int) * 2);
-//		if (!pipes[i])
-//			return (error_msg_return_int(MSG_ERR_MEM, NULL, malloc_error,
-//										 1));
-//		i++;
-//	}
-//	return (pipes);
-//}
-
-void	begin_pipes(t_cmd *cmd, int len)
-{
-	t_cmd	*tmp;
-
-	tmp = cmd;
-//	while(tmp)
-//	{
-//		tmp = tmp->next
-//	}
-}
-
 void	multi_pipe_process(t_list **envlst, t_list *cmdlst)
 {
-//	int		len;
-//	int 	**pipes;
-//	int		i;
 	t_cmd	*tmp;
 	t_cmd	*prev;
 	t_fd	fds;
+	int		i;
 
-//	len = ft_lstsize((t_list *)cmd);
-//	pipes = NULL;
-//	pipes = make_pipe_space(pipes, len);
-//	begin_pipes(cmd, len);
 	prev = NULL;
-	if (pipe(fds.fd1) == -1)
-		error_msg_return_void(MSG_SYSTEM_ERR_PIPE, NULL, pipe_error, 1);
-	if (pipe(fds.fd2) == -1)
-		error_msg_return_void(MSG_SYSTEM_ERR_PIPE, NULL, pipe_error, 1);
-	while (cmdlst)
+	i = 0;
+	while (cmdlst && ++i)
 	{
 		tmp = (t_cmd *)(cmdlst->content);
 		if (prev == NULL)
 			exec_first_cmd(*envlst, tmp, &fds);
 		else if (cmdlst->next == NULL)
-			exec_last_cmd(*envlst, tmp, &fds);
+			exec_last_cmd(*envlst, tmp, &fds, i);
 		else
-			exec_middle_cmd(*envlst, tmp, &fds);
+			exec_middle_cmd(*envlst, tmp, &fds, i);
 		prev = tmp;
 		cmdlst = cmdlst->next;
 	}
 	while (wait(0) != -1)
 		;
-	printf("parent finish\n");
 }
